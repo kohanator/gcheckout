d96afe63636f5f89aa2f93cfb26842c621675d20
Woody Gilk <woody.gilk@kohanaphp.com>
2009-10-04 16:37:24 -0500

    Created base gCheckout class with class comment

---
+++ classes/kohana/gcheckout.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * @package    Google Checkout
+ * @copyright  (c) 2009 Woody Gilk
+ * @author     Woody Gilk
+ * @license    MIT
+ */
+class Kohana_gCheckout {
+
+
+} // End gCheckout
